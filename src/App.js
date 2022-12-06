805f4a252ab30417448a0c6a2e6a258a69b344b0
Mustafa Koçer <mkocerim@gmail.com>
2022-12-06 22:52:30 +0300

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import City from "./city";
 
 function App() {
+  const key = "7977a76f2108c9a33f10d288ed8a9b0f";
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  }, [search]);
+
+  console.log(search);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          className="my-5 text-center px-3 py-3 w-[250px] placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-0 outline-none focus:outline-none focus:ring"
+          type="text"
+          placeholder="placeholder"
+        />
+        <City city={city} />
+      </div>
     </div>
   );
 }
---
+++ src/city.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const City = ({city}) => {
+  console.log();
+
+  return (
+    <div>
+      <div>
+      <h1 className="text-5xl">{city.name}</h1>
+        <h1 className="text-3xl">{city.main.temp}</h1>
+        <h1 className="text-3xl">{city.weather[0].main}</h1>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default City;
